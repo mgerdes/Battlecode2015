9157ec78edb531c44b468b7af34d20a7130349ac
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-12-27 10:55:05 -0500

    Formatted file and removed unused direction array

--- teams/Navigation/RobotPlayer.java
+++ teams/Navigation/RobotPlayer.java
@@ -4,29 +4,27 @@ import battlecode.common.*;
 
 public class RobotPlayer {
 
-	public static void run(RobotController rc) {
-		Direction[] directions = {Direction.NORTH, Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST, Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST};
+    public static void run(RobotController rc) {
+        while (true) {
+            if (rc.getType() == RobotType.HQ) {
+                try {
+                    //Check if a robot is spawnable and spawn one if it is
+                    if (rc.isActive() && rc.senseRobotCount() < 25) {
+                        Direction toEnemy = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
 
-		while(true) {
-			if (rc.getType() == RobotType.HQ) {
-				try {					
-					//Check if a robot is spawnable and spawn one if it is
-					if (rc.isActive() && rc.senseRobotCount() < 25) {
-						Direction toEnemy = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
+                        if (rc.senseObjectAtLocation(rc.getLocation().add(toEnemy)) == null) {
+                            rc.spawn(toEnemy);
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("HQ Exception");
+                }
+            }
 
-						if (rc.senseObjectAtLocation(rc.getLocation().add(toEnemy)) == null) {
-							rc.spawn(toEnemy);
-						}
-					}
-				} catch (Exception e) {
-					System.out.println("HQ Exception");
-				}
-			}
-			
-			if (rc.getType() == RobotType.SOLDIER) {
-				Soldier.init(rc);
-				Soldier.run();
-			}
-		}
-	}
+            if (rc.getType() == RobotType.SOLDIER) {
+                Soldier.init(rc);
+                Soldier.run();
+            }
+        }
+    }
 }
