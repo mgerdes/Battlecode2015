56c2c98de3f307f930c590dbb70e36c1c217d392
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-12-27 13:16:55 -0500

    Helper for setting indicator strings
    Message queue will start at position 0 each round and fill positions as needed.
    
    If more than three messages are set in a round, the oldest message is overwritten. A message number is included to identify when this happens.

---
+++ teams/Navigation/Debug.java
@@ -0,0 +1,20 @@
+package Navigation;
+
+import battlecode.common.Clock;
+import battlecode.common.RobotController;
+
+public class Debug {
+    private static int previousRound = 0;
+    private static int channel = 0;
+
+    public static void setIndicatorString(String message, RobotController rc) {
+        int roundNumber = Clock.getRoundNum();
+        if (roundNumber != previousRound) {
+            channel = 0;
+            previousRound = roundNumber;
+        }
+
+        rc.setIndicatorString(channel % 3, String.format("round %d message %d: %s", Clock.getRoundNum(), channel, message));
+        channel++;
+    }
+}
