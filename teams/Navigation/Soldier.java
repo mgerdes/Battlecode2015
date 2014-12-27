ec0ece3b9c6d4a52df0bca88d5e9a236e3bd780c
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-12-27 11:37:55 -0500

    Testing navigation on Apartments...

--- teams/Navigation/Soldier.java
+++ teams/Navigation/Soldier.java
@@ -5,7 +5,7 @@ import battlecode.common.*;
 //--This is a dumb soldier used to test the bug nav
 public class Soldier {
     private static RobotController rc;
-    private static final MapLocation TEST_DESTINATION = new MapLocation(15, 26);
+    private static final MapLocation TEST_DESTINATION = new MapLocation(19, 20);
 
     public static void init(RobotController rcC) {
         rc = rcC;
