26ef940fa5a6acecbe35e8596e3875e2b05a453c
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-12-27 14:26:22 -0500

    Check if mapLocation is valid.
    Also, array access uses row/col form

--- teams/Navigation/CachedMap.java
+++ teams/Navigation/CachedMap.java
@@ -5,22 +5,32 @@ import battlecode.common.*;
 public class CachedMap {
     private static RobotController rc;
     private static TerrainTile[][] map;
+    private static int mapHeight;
+    private static int mapWidth;
 
     public static void init(RobotController rcC) {
         rc = rcC;
-        map = new TerrainTile[rc.getMapWidth()][rc.getMapHeight()];
+        mapHeight = rc.getMapHeight();
+        mapWidth = rc.getMapWidth();
+        map = new TerrainTile[mapHeight][mapWidth];
     }
 
     public static TerrainTile getTile(MapLocation location) {
-        if (map[location.x][location.y] == null) {
-            map[location.x][location.y] = rc.senseTerrainTile(location);
+        if (map[location.y][location.x] == null) {
+            map[location.y][location.x] = rc.senseTerrainTile(location);
         }
 
-        return map[location.x][location.y];
+        return map[location.y][location.x];
     }
 
     public static boolean isNavigable(MapLocation location, Direction direction) {
         MapLocation next = location.add(direction);
+        if (next.x < 0
+                || next.y < 0
+                || next.x >= mapWidth
+                || next.y >= mapHeight) {
+            return false;
+        }
         return CachedMap.getTile(next) != TerrainTile.VOID;
     }
 }
