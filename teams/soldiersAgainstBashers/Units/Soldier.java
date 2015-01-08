56af3e96075e824431b91f47a83d05f76bb2c7db
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2015-01-07 21:17:46 -0500

    Updating soldier target priority
    if an enemy is close enough, it becomes a priority target regardless of its health

--- teams/soldiersAgainstBashers/Units/Soldier.java
+++ teams/soldiersAgainstBashers/Units/Soldier.java
@@ -35,12 +35,18 @@ public class Soldier {
     static void doYourThing() throws GameActionException {
         RobotInfo[] enemies = rc.senseNearbyRobots(attackRadius, badGuys);
         if (enemies.length > 0 && rc.isWeaponReady()) {
+            MapLocation currentLocation = rc.getLocation();
             double health = 10000;
             int index = 0;
             for (int i = 0; i < enemies.length; i++) {
                 if (enemies[i].health < health) {
                     health = enemies[i].health;
                     index = i;
+                }
+
+                if (enemies[i].location.distanceSquaredTo(currentLocation) < 4) {
+                    index = i;
+                    break;
                 }
             }
 
