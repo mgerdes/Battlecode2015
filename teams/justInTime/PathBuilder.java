890f7e4e6b8ccad04ee52de01c6286602cb6df9f
Michael Gerdes <mgerdes11@gmail.com>
2015-01-18 13:37:05 -0500

    Added queue to channel list

--- teams/justInTime/PathBuilder.java
+++ teams/justInTime/PathBuilder.java
@@ -1,17 +1,23 @@
 package justInTime;
 
+import battlecode.common.GameActionException;
+import justInTime.constants.ChannelList;
 import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
 
 public class PathBuilder {
+    static RobotController rc;
+
     public static void setup(MapLocation[] towerList, MapLocation enemyHq) {
         //--Only the HQ should call this
         //--This should only be called once
 
         //--broadcast all of destinations (point of interests)
         //--broad the number of POI
+
     }
 
-    public static void build(int bytecodeLimit) {
+    public static void build(int bytecodeLimit) throws GameActionException {
         //--Read the state (last location and point of interest number)
         //--Do the work and broadcast the work
         //--Broadcast the state
