52bec4424822a23a21210c15f86ee34516a71976
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2015-01-21 16:44:28 -0500

    More elaborate Drone micro
    It all depends on how many enemies can shoot it, and how many of our teammates can shoot that set.
    
    These micro changes led to a 60/40 advantage over justInTime
    
    I fixed a mistake in the Helper.getDirection.. method. I was setting the value instead of using +=.
    
    I added a BasicNavigation class to handle the tryMove method that I have been putting in so many classes.

---
+++ teams/warble/navigation/BasicNav.java
@@ -0,0 +1,27 @@
+package warble.navigation;
+
+import battlecode.common.Direction;
+import battlecode.common.RobotController;
+import warble.util.Helper;
+
+public class BasicNav {
+    private static RobotController rc;
+
+    private static final int[] directions = new int[]{0, -1, 1, -2, 2};
+
+    public static void init(RobotController rcC) {
+        rc = rcC;
+    }
+
+    public static Direction getNavigableDirectionClosestTo(Direction initial) {
+        int initialDirectionValue = Helper.getInt(initial);
+        for (int i = 0; i < directions.length; i++) {
+            Direction direction = Helper.getDirection(initialDirectionValue + i);
+            if (rc.canMove(direction)) {
+                return direction;
+            }
+        }
+
+        return Direction.NONE;
+    }
+}
